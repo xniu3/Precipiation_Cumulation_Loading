abf518792bd837712d8c017857424c43651c060d
xinhaoniu <xniu3@ualberta.ca>
2022-12-19 19:20:09 -0800

    Second Addition

--- utils/getPrecipByYear.py
+++ utils/getPrecipByYear.py
@@ -1,17 +1,18 @@
 import json
 import sys
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
 import urllib
 
 
-def get_precip_by_year(year:int) -> list:
+def get_precip_by_year(year: int) -> list:
     try:
         ResultBytes = urllib.request.urlopen(
             "https://weather.visualcrossing.com/" +
             "VisualCrossingWebServices/rest/services/timeline/" +
             "280%20Vanderbilt%20Beach%20Rd%2C%20Naples%2C%20FL%2034108/" +
-             str(year) + "-12-11/" + str(year) + "-12-15" +
+            str(year) + "-12-11/" + str(year) + "-12-15" +
             "?unitGroup=metric&key=JSU4FDB6ZE6EN59D753EPKHJG&contentType=json")
         # Parse the results as JSON
         jsonData = json.loads(ResultBytes.read().decode('utf-8'))
