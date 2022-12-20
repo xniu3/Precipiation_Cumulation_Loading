868f8ff4659cdbaa1c8daef6ae45625725652822
xniu3 <96604983+xniu3@users.noreply.github.com>
2022-12-19 22:16:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     """Get the response from Visual Crossing."""
-    Precip_2019 = [5.196, 16.822, 3.301, 0.254, 0.0] # getPrecipByYear.get_precip_by_year(2019)
-    Precip_2020 = [0.0, 0.0, 0.0, 0.0, 0.0] # getPrecipByYear.get_precip_by_year(2020)
-    Precip_2021 = [0.0, 0.0, 0.0, 0.744, 0.857]# getPrecipByYear.get_precip_by_year(2021)
-    Precip_2022 = [0.0, 0.0, 0.0, 0.0, 5.861] # getPrecipByYear.get_precip_by_year(2022)
+    Precip_2019 = getPrecipByYear.get_precip_by_year(2019)
+    Precip_2020 = getPrecipByYear.get_precip_by_year(2020)
+    Precip_2021 = getPrecipByYear.get_precip_by_year(2021)
+    Precip_2022 = getPrecipByYear.get_precip_by_year(2022)
     if sum(Precip_2022) / len(Precip_2022) >= (sum(Precip_2021) / len(Precip_2021) + sum(Precip_2020) / len(Precip_2020) + sum(Precip_2019) / len(Precip_2019)) / 3:
         return f"The project should be delayed. \n", 200
     else:
