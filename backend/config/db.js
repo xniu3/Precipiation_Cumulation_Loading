f7cdba48f977fc489edbcc6be159e74cfd770dc0
xniu3 <96604983+xniu3@users.noreply.github.com>
2023-01-15 00:54:50 +0000

    Fixed bugs

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import data from "./default.json" assert { type: "json" };
+// console.log(`db is ${data.mongoURI}`);
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        });
+        console.log("MongoDB connected ... ");
+    } catch (err) {
+        console.error(err.message);
+        // Exit process with failure
+        process.exit(1)
+    }
+}
+
+//mongoose.connect(data.mongoURI);
+
+export default connectDB;
+// module.exports = connectDB;
+
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,20 @@
-//configure an expres file 
-// .env file is used to save the environment variables. 
+import express from 'express';
+import connectDB from './config/db.js';
+import PromiseProvider from 'mongoose';
+import dotenv from "dotenv";
 
-import express from "express";
-import cors from "cors";
-import weather from "./api/weather.route.js";
-import welcome from "./api/welcome.route.js"
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
 
-// configure middle ware
-app.use(cors());
-app.use(express.json);// our server can accept json in the body of the request. 
-// any request could be re-jsoned. 
-app.use("/", welcome);
-app.use("/api/v1/weather", weather);
-app.use("*", (req, res) => res.status(404).json({error: "Not Found!"}));
+app.get('/', (req, res) => {
+    res.send('API is running');
+})
 
+app.listen( PORT, () =>{
+    console.log(`Server started on port ${PORT}`);
+});
 
 export default app;
-
-
-
-
-
